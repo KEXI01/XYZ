8b822e2917c3452489d33803118cf48cef5f47a7
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-04-20 20:13:15 +0530

    Update mustjoin.py

--- Opus/plugins/bot/mustjoin.py
+++ Opus/plugins/bot/mustjoin.py
@@ -1,35 +1,33 @@
-import random
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from Opus import app
 
-MUST_JOIN = "STORM_CORE"
+MUST_JOIN_CHANNEL = "STORM_TECHH"
 
-@app.on_message(filters.incoming & filters.private, group=-1)
+@app.on_message(filters.incoming & group=-1)
 async def must_join_channel(app: Client, msg: Message):
-    if not MUST_JOIN:
+    if not MUST_JOIN_CHANNEL:
         return
     try:
         try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await app.get_chat_member(MUST_JOIN_CHANNEL, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            if MUST_JOIN_CHANNEL.isalpha():
+                invite_link = f"https://t.me/{MUST_JOIN_CHANNEL}"
             else:
-                chat_info = await app.get_chat(MUST_JOIN)
-                link = chat_info.invite_link
+                chat_info = await app.get_chat(MUST_JOIN_CHANNEL)
+                invite_link = chat_info.invite_link
+
             try:
                 await msg.reply_text(
                     text=(
-                        "ʜᴇʏ 🎧\n"
-                        "» ʀᴇᴀᴅʏ ᴛᴏ ᴠɪʙᴇ? ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ, ᴊᴏɪɴ ᴜꜱ ᴛᴏ ꜱᴛᴀʀᴛ ᴀɴᴅ ᴜꜱᴇ ᴍʏ ꜰᴇᴀᴛᴜʀᴇꜱ 🚀"
+                        "<blockquote><b>» ᴛᴏ ᴜꜱᴇ ᴍʏ ꜰᴇᴀᴛᴜʀᴇꜱ, ʏᴏᴜ ᴍᴜꜱᴛ ᴊᴏɪɴ ᴏᴜʀ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ғɪʀꜱᴛ.</b></blockquote>"
                     ),
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇ", url="https://t.me/STORM_TECHH"),
-                                InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/STORM_CORE"),
+                                InlineKeyboardButton("📢 ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=invite_link),
                             ]
                         ]
                     )
@@ -38,4 +36,4 @@ async def must_join_channel(app: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"» ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_ᴊᴏɪɴ ᴄʜᴀᴛ ~ {MUST_JOIN}")
+        print(f"» ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀꜱ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ~ {MUST_JOIN_CHANNEL}")
