0a4eb635161992dd5e8727a9ee66f8dfec313a81
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-05-02 05:04:46 +0530

    Update help.py

--- Opus/utils/inline/help.py
+++ Opus/utils/inline/help.py
@@ -30,19 +30,17 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
-                ),  
-            ],
-            [
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
-                ), 
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
                 ),                
             ],
             [
+                InlineKeyboardButton(
+                    text=_["H_B_5"],
+                    callback_data="help_callback hb5",
+                ),
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb6",
@@ -52,6 +50,7 @@ def help_pannel(_, START: Union[bool, int] = None):
                     callback_data="help_callback hb7",
                 ),                
             ],
+
             [
                 InlineKeyboardButton(
                     text=_["H_B_8"],
