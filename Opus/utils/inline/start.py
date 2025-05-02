c62fee7c0d68ed2b34ebfc542262e5e1d12eb290
K Σ X ! <167321927+KEXI01@users.noreply.github.com>
2025-05-02 17:16:04 +0530

    Update start.py

--- Opus/utils/inline/start.py
+++ Opus/utils/inline/start.py
@@ -27,6 +27,5 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),           
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]      
     return buttons
